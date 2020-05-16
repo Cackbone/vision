955b67857e1980a6dcd92625832ce71e1d8b0b05
Cedric Legendre <cedric.legendre@epitech.eu>
2020-05-17 00:18:48 +0200

    (feat) [core]: Add Vision struct with images loader (image crate)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+use std::error::Error;
+
+use image::{io::Reader, DynamicImage};
+
+pub struct Vision {
+    images: Vec<DynamicImage>,
+}
+
+impl Vision {
+    pub fn new() -> Self {
+        Vision {
+            images: Vec::new()
+        }
+    }
+
+    pub fn load(&mut self, file: &str) -> Result<(), Box<dyn Error>> {
+        let image = Reader::open(file)?.with_guessed_format()?.decode()?;
+
+        self.images.push(image);
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
