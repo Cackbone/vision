0c1741e69898f7712ed69aa33258b6d295e16607
Cedric Legendre <cedric.legendre@epitech.eu>
2020-05-17 03:05:48 +0200

    (feat) [colors]: Add function to get main colors

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
+use std::collections::HashMap;
 
-use image::{io::Reader, DynamicImage};
+use image::{io::Reader, DynamicImage, Pixel, Rgba};
 
 #[derive(Clone)]
 pub struct VisionImage(DynamicImage);
@@ -10,6 +11,21 @@ impl VisionImage {
         let dyn_image = Reader::open(path)?.with_guessed_format()?.decode()?;
 
         Ok(VisionImage(dyn_image))
+    }
+
+
+    pub fn get_main_colors(&self, colors_nb: usize) -> Vec<Rgba<u8>> {
+        let rgba = self.0.to_rgba();
+        let mut counter: HashMap<Rgba<u8>, usize> = HashMap::new();
+
+        for pixel in rgba.pixels() {
+            *counter.entry(*pixel).or_insert(0) += 1;
+        }
+
+        let mut counter_sorted: Vec<(&Rgba<u8>, &usize)> = counter.iter().collect();
+        counter_sorted.sort_by(|a, b| b.1.cmp(a.1));
+
+        counter_sorted.iter().take(colors_nb).map(|x| *x.0).collect()
     }
 }
 
@@ -24,15 +40,39 @@ impl Vision {
         }
     }
 
-    pub fn load(&mut self, path: &str) -> Result<(), Box<dyn Error>> {
+    pub fn load_one(&mut self, path: &str) -> Result<(), Box<dyn Error>> {
         let image = VisionImage::load(path)?;
 
         self.add(image);
         Ok(())
     }
 
+    pub fn load(&mut self, paths: Vec<&str>) -> Result<(), Box<dyn Error>> {
+        for path in paths {
+            self.load_one(path)?
+        }
+        Ok(())
+    }
+
     pub fn add(&mut self, image: VisionImage) {
         self.images.push(image)
+    }
+
+    pub fn get_main_colors(&self, colors_nb: usize) -> Vec<Rgba<u8>> {
+        let mut counter: HashMap<Rgba<u8>, usize> = HashMap::new();
+
+        for image in self.images.iter() {
+            let rgba = image.0.to_rgba();
+
+            for pixel in rgba.pixels() {
+                *counter.entry(*pixel).or_insert(0) += 1;
+            }
+        }
+
+        let mut counter_sorted: Vec<(&Rgba<u8>, &usize)> = counter.iter().collect();
+        counter_sorted.sort_by(|a, b| b.1.cmp(a.1));
+
+        counter_sorted.iter().take(colors_nb).map(|x| *x.0).collect()
     }
 }
 
